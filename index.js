d85fdaf0bd2a740df26fd22df1e54d7553fb7266
Samuel <ingenierofiestero@gmail.com>
2018-06-27 10:50:01 +0200

    allow update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ server.get('/',(req,res,next)=>{
     res.send('hello webward');
     next();
 })
-/*
+
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
@@ -115,4 +115,4 @@ function decrypt(text) {
     var dec = decipher.update(text, 'hex', 'utf8')
     dec += decipher.final('utf8');
     return dec;
-}*/
+}
