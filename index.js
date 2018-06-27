3bd22758cf090e434fa8baf0003bc2a052cd02d9
Samuel <ingenierofiestero@gmail.com>
2018-06-27 11:33:31 +0200

    changes

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ server.get('/',(req,res,next)=>{
     res.send('hello webward');
     next();
 })
-
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
-
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector).set('storage', inMemoryStorage);
 
